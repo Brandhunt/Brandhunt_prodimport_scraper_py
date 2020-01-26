67070b204afb66ced7ff714d1ad380501b3ca1a4
Brandhunt <57029610+Brandhunt@users.noreply.github.com>
2020-01-26 07:39:07 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -208,7 +208,8 @@ for scrapsite in jsonscrapsites:
         root = ''
         nextURLs = ''
         try:
-            html = scraperwiki.scrape(scrapsite['scrapeurl'])
+            html = scraperwiki.scrape(scrapsite['scrapeurl'],\
+                   user_agent='Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36')
             #print("HTML:")
             #print(html)
         except HTTPError as err:

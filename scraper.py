a1dd0f1ab4353b500954b489108598a605fd1995
Brandhunt <57029610+Brandhunt@users.noreply.github.com>
2020-01-26 17:35:46 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -701,12 +701,8 @@ for scrapsite in jsonscrapsites:
                                     #output = re.finditer(r'image\=\"(.*)\"', image_dom, flags=re.U)
                                     output = re.finditer(r'image\=\"(.*?)\"', image_dom)
                                     array_output = []
+                                    print(output.group(1))
                                     for output_el in output:
-                                        
-                                        print('HEPP')
-                                        print(output_el.group())
-                                        print('HUPP')
-                                        
                                         array_output.append(output_el.group())
                                     if len(array_output) > 1:
                                         if array_output[1].len() > 0:
